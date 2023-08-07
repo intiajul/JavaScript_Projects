d3d2f983763727bd157465e625eda4ed723df090
intiajul <intiajulalam16@gmail.com>
2023-08-07 14:40:57 +0600

    Added successfully.

---
+++ grade.js
@@ -0,0 +1,27 @@
+let n = prompt("Enter your grade: ");
+
+if (n >= 97 && n <= 100) {
+    console.log("A+:4.0"); 
+        } else if (n >= 90 && n <= 96) {
+            console.log("A:4.0");
+        } else if (n >= 85 && n <= 89) {
+            console.log("A-:3.7");
+        } else if (n >= 80 && n <= 84) {
+            console.log("B+:3.3");
+        } else if (n >= 75 && n <= 79) {
+            console.log("B:3.0");
+        } else if (n >= 70 && n <= 74) {
+            console.log("B-:2.7");
+        } else if (n >= 65 && n <= 69) {
+            console.log("C+:2.3");
+        } else if (n >= 60 && n <= 64) {
+            console.log("C:2.0");
+        } else if (n >= 55 && n <= 59) {
+            console.log("D+:1.3");
+        } else if (n >= 50 && n <= 54) {
+            console.log("D:0.7");
+
+        } else {
+            console.log("Failed!");
+        }
+    
