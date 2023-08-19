48f5d1bc908c81f69a7ba2eaab9a7e00afbfb522
intiajul <intiajulalam16@gmail.com>
2023-08-20 02:05:57 +0600

    Updated.

--- calculateAge.js
+++ calculateAge.js
@@ -1,18 +1,17 @@
-
-//input
+//Take input
 let n = prompt("Please, enter your birthday (mm-dd-yyyy): ");
 
 
-//split
+//split the input
 let parts = n.split("-");
 let month = parseInt(parts[0]);
 let day = parseInt(parts[1]);
 let year = parseInt(parts[2]);
 
-//new formate
+//new formated 
 let formatted = year + "-" + month + "-" + day;
 
-//class declare
+//class declaration
 class person {
     constructor(dob) {
         this.birthday = dob;
@@ -26,7 +25,7 @@ class person {
     }
 }
 
-
+//function calling
 let n1 = new person(formatted);
 
 console.log(n1.calculateAge());
